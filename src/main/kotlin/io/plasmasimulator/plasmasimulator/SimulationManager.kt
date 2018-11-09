64ae80fda781ec4754d3908d08109c3fb712b07a
Georgi Aylov <georgi.aylov@tum.de>
2018-11-09 14:40:55 +0100

    chore: add logback logger

--- src/main/kotlin/io/plasmasimulator/plasmasimulator/SimulationManager.kt
+++ src/main/kotlin/io/plasmasimulator/plasmasimulator/SimulationManager.kt
@@ -2,8 +2,14 @@ package io.plasmasimulator.plasmasimulator
 
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
+import org.slf4j.LoggerFactory
 
 class SimulationManager : AbstractVerticle() {
+
+  private companion object {
+    private val LOGGER = LoggerFactory.getLogger(SimulationManager::class.java)
+  }
+
   override fun start(startFuture: Future<Void>) {
     vertx
       .createHttpServer()
@@ -15,7 +21,7 @@ class SimulationManager : AbstractVerticle() {
       .listen(8080) { http ->
         if (http.succeeded()) {
           startFuture.complete()
-          println("HTTP server started on port 8080")
+          LOGGER.info("HTTP server started on port 8080")
         } else {
           startFuture.fail(http.cause())
         }
