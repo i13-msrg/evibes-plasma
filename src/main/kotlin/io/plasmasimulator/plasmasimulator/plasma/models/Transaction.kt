2f715319bddf79dd0e31aec496b014d4922ffd02
Georgi Aylov <georgi.aylov@tum.de>
2018-11-17 20:15:10 +0100

    feat: calculate transaction hash

--- src/main/kotlin/io/plasmasimulator/plasmasimulator/plasma/models/Transaction.kt
+++ src/main/kotlin/io/plasmasimulator/plasmasimulator/plasma/models/Transaction.kt
@@ -1,11 +1,15 @@
 package io.plasmasimulator.plasmasimulator.plasma.models
 
+import java.security.MessageDigest
+
 class Transaction {
+  val digest: MessageDigest = MessageDigest.getInstance("SHA-256")
+  var hash = mutableListOf<Byte>()
   var inputs = mutableListOf<Input>()
   var outputs = mutableListOf<Output>()
 
   class Input(val blockNum: Int, val txIndex: Int, val outputIndex: Int, val sig: String = "")
-  class Output(address: String, amount: Int)
+  class Output(val address: String, val amount: Int)
 
   fun addInput(blockNum: Int, txIndex: Int, outputIndex: Int) {
     inputs.add(Input(blockNum, txIndex, outputIndex))
@@ -13,6 +17,27 @@ class Transaction {
 
   fun addOutput(address: String, amount: Int) {
     outputs.add(Output(address, amount))
+  }
+
+  fun getTxHash() : List<Byte> {
+    if(hash.size > 0) return hash
+
+    var tx = mutableListOf<Byte>()
+
+    for(input in inputs) {
+      tx.add(input.blockNum.toByte())
+      tx.add(input.txIndex.toByte())
+      tx.add(input.outputIndex.toByte())
+    }
+    for(output in outputs) {
+      tx.add(output.amount.toByte())
+      tx.add(output.address.toByte())
+    }
+    digest.update(tx.toByteArray())
+
+    hash = digest.digest().toMutableList()
+
+    return hash
   }
 
 }
