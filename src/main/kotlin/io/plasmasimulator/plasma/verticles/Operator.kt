5244fbb3c2cc7b7924df4490dde94764974067cc
Georgi Aylov <georgi.aylov@tum.de>
2018-11-26 13:29:18 +0100

    chore: add block creation from operator

--- src/main/kotlin/io/plasmasimulator/plasma/verticles/Operator.kt
+++ src/main/kotlin/io/plasmasimulator/plasma/verticles/Operator.kt
@@ -3,12 +3,16 @@ package io.plasmasimulator.plasma.verticles
 import io.plasmasimulator.conf.Address
 import io.plasmasimulator.plasma.models.PlasmaBlock
 import io.plasmasimulator.plasma.models.PlasmaChain
+import io.plasmasimulator.plasma.models.Transaction
 import io.plasmasimulator.utils.MerkleTreeBuilder
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
+import io.vertx.core.json.Json
 import org.slf4j.LoggerFactory
 
 class Operator: PlasmaParticipant() {
+  var transactions = mutableListOf<Transaction>()
+  val TRANSACTIONS_PER_BLOCK = 10
 
   private companion object {
     private val LOG = LoggerFactory.getLogger(PlasmaParticipant::class.java)
@@ -16,14 +20,34 @@ class Operator: PlasmaParticipant() {
 
   override fun start(startFuture: Future<Void>?) {
     super.start(startFuture)
+    vertx.eventBus().consumer<Any>(Address.PUBLISH_TRANSACTION.name) { msg ->
+      val newTransaction = Json.decodeValue(msg.body().toString(), Transaction::class.java)
+      // TODO: should transaction be validated before added to the block
+      transactions.add(newTransaction)
+      if(transactions.size == TRANSACTIONS_PER_BLOCK)
+        createBlock(transactions)
+      val newBlock: PlasmaBlock = Json.decodeValue(msg.body().toString(), PlasmaBlock::class.java)
+      applyBlock(newBlock)
+    }
+  }
+
+  fun createBlock(transactions: List<Transaction>) : PlasmaBlock{
+      val prevBlock = chain.getLastBlock()
+      val newBlock = PlasmaBlock(number = prevBlock.number + 1,
+                                 prevBlockNum = prevBlock.number,
+                                 prevBlockHash = prevBlock.hash.toByteArray(),
+                                 transactions = transactions)
+      val blockRoot = MerkleTreeBuilder.getRoot(newBlock.transactions)
+      newBlock.merkleRoot = blockRoot.digest.toMutableList()
+
+      return newBlock
 
 
   }
 
   fun applyBlock(block: PlasmaBlock) : Boolean {
     if(!chain.validateBlock(block)) return false
-    val blockRoot = MerkleTreeBuilder.getRoot(block.transactions)
-    send(Address.APPLY_BLOCK.name, blockRoot.digest)
+    send(Address.APPLY_BLOCK.name, block.merkleRoot)
     return true
   }
 
